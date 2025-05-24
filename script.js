705fbdb2539238edc4b01bf17c8bf42208c1a4fa
janfdev <fauzanrizqinoor@gmail.com>
2025-05-24 07:48:59 +0700

    Refactor navigation structure and styles; implement scroll-to-top functionality

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-le
+let nav = document.querySelector("nav");
+let scrollToTopButton = document.querySelector(".scroll-to-top");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollToTopButton.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollToTopButton.style.display = "none";
+  }
+};

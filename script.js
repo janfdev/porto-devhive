c2ae9b078d3b7b46de6be480e3169910cf851e5f
janfdev <fauzanrizqinoor@gmail.com>
2025-05-24 10:20:16 +0700

    Add Bootstrap skill image and enhance skills section layout and styling

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ window.onscroll = function () {
     scrollToTopButton.style.display = "none";
   }
 };
+
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollToTopButton.style.pointerEvents = "none";
+};
+
+const hideNavbarMenu = () => {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+};
+
+// Close side navigation
+cancelBtn.onclick = hideNavbarMenu;
+
+// Close side navigation when a menu link is clicked
+let navLinks = document.querySelectorAll(".menu li a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideNavMenu);
+});
